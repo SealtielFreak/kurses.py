3c77a07a6773b33923b374cd98de5ba9356fe5b4
SealtielFreak <die.seal_valderrama@outlook.com>
2023-05-14 02:35:07 -0600

    a20110339 It's me, and clean Pygame class for abstract methods
    Signed-off-by: a20110339 <a20110339@ceti.mx>

--- pyrogue/backend/pygame.py
+++ pyrogue/backend/pygame.py
@@ -1,7 +1,35 @@
 import pyrogue.virtual_console
+from pyrogue.virtual_console import T
 
 
 class PygameVirtualConsole(pyrogue.virtual_console.VirtualConsole):
+    def keyspressed(self) -> list[chr]:
+        pass
+
+    @property
+    def window(self) -> T:
+        pass
+
+    @property
+    def surface(self) -> T:
+        pass
+
+    @property
+    def font(self) -> T:
+        pass
+
+    def clear_cache(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def events(self):
+        pass
+
+    def quit(self):
+        pass
+
     @property
     def buffer(self) -> pyrogue.buffer_matrix.BufferTerm:
         pass
